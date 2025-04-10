29d3c035a2ae7145d7104edb701f8404a61910a4
twitch10126 <twitch10126@hotmail.com>
2025-04-10 11:25:35 +0100

    Add jshint config setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
+/* jshint esversion: 11 */
+
 // Taken from Boardwalk games project 
-   document
-       .querySelectorAll(".navbar-collapse .nav-link")
-       .forEach((link) => {
-           link.addEventListener("click", function (e) {
-               let section = document.querySelector(e.target.getAttribute("href"));
-               if (section) {
-                   e.preventDefault(); // Prevent default anchor click behavior
-                   let navbarHeight = document.querySelector(".navbar-toggler").offsetHeight;
-                   window.scroll({
-                       top: section.offsetTop - navbarHeight, // Adjust for navbar height
-                       behavior: "smooth",
-                   });
-                   document
-                       .querySelector(".navbar-collapse")
-                       .classList.remove("show"); // Collapse navbar
-               }
-           });
-       });
+document
+.querySelectorAll(".navbar-collapse .nav-link")
+.forEach((link) => {
+    link.addEventListener("click", function (e) {
+        let section = document.querySelector(e.target.getAttribute("href"));
+        if (section) {
+            e.preventDefault(); // Prevent default anchor click behavior
+            let navbarHeight = document.querySelector(".navbar-toggler").offsetHeight;
+            window.scroll({
+                top: section.offsetTop - navbarHeight, // Adjust for navbar height
+                behavior: "smooth",
+            });
+            document
+                .querySelector(".navbar-collapse")
+                .classList.remove("show"); // Collapse navbar
+        }
+    });
+});
